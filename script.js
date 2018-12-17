5ab721abc27b1bd9df00efa42130f071a27e4296
Peng Xue Vang <pxvang33@gmail.com>
2018-12-17 02:52:50 -0600

    update

--- script.js
+++ script.js
@@ -25,27 +25,42 @@ function clickSubmit(){
     clearnInputs();
 
 }
-
+// function which appends input fields onto the dom, will place function inside of 'clickSubmit' function
 function appendEmployee(){
+    // think I can declare each id input as its own variable instead of writing it fully out
     let employee = new Employees($('#firstNameIn').val(), $('#lastNameIn').val(), $('#idIn').val(), $('#titleIn').val(), $('#annualSalaryIn').val());
     console.log(employee);
+    let totalMonthly = 0
     // pushing employee into array to loop through and then display each index onto the DOM
     employeesArray.push(employee);
-    $('#empShow').append(
+    $('#firstRow').empty();
+
+    for( person of employeesArray){
+    $('#firstRow').append(
     '<tr>'+ '<td>' + employee.firstName + '</td>'
     + '<td>' + employee.lastName + '</td>' 
     + '<td>' + employee.id + '</td>' 
     +  '<td>' + employee.title + '</td>' 
-    + '<td>' + employee.annualSalary + '</td>' + '</tr>');
+    + '<td>' + Number(employee.annualSalary).toFixed(2) + '</td>' + '</tr>');
+        
+    totalMonthly += employee.annualSalary
 
+    }
+    
+    console.log('total expense', totalMonthly);
 } 
 
 function clearnInputs(){
+    // created function to clear input fields after clicking button
     $('#firstNameIn').val('');
     $('#lastNameIn').val('');
     $('#idIn').val('');
     $('#titleIn').val('');
     $('#annualSalaryIn').val('');
+
+}
+
+function updateTotalMonthly(){
 
 }
 
