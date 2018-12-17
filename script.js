92e0919778e879921c6d62384488d25b667e3469
Peng Xue Vang <pxvang33@gmail.com>
2018-12-17 06:35:44 -0600

    update end

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ class Employees {
 $(document).ready(function(){
     console.log('ready');
     $('#clicker').on('click', clickSubmit );
+    $('#empShow').on('click', '.delete', removeRow);
     // $('#empShow1').append('<tr>' +
     //     '<th>  First Name  </th>' 
     //     + '<th>  Last Name  </th>'
@@ -29,7 +30,6 @@ function clickSubmit(){
     console.log('button clicked');
     // declaring variable with employee constructor and swapping properities with input id's to obtian their value
     appendEmployee();
-
     clearInputs();
 
 }
@@ -38,7 +38,7 @@ function appendEmployee(){
     // think I can declare each id input as its own variable instead of writing it fully out
     let employee = new Employees($('#firstNameIn').val(), $('#lastNameIn').val(), $('#idIn').val(), $('#titleIn').val(), $('#annualSalaryIn').val());
     console.log(employee);
-    let totalMonthly = 0
+    let totalMonthly = 0;
     // pushing employee into array to loop through and then display each index onto the DOM
     employeesArray.push(employee);
     // don't want to empty the whole table how to stop?? created tbody to solve issue
@@ -47,7 +47,7 @@ function appendEmployee(){
     for( let i = 0; i < employeesArray.length; i++){
     // appending the index properties of array
     $('#tbody').append(
-    '<tr>'+ '<td>' + employeesArray[i].firstName + '</td>'
+    '<tr>'+ '<td>'+'<button class ="delete">X</button>' + ' ' + employeesArray[i].firstName + '</td>'
     + '<td>' + employeesArray[i].lastName + '</td>' 
     + '<td>' + employeesArray[i].id + '</td>' 
     +  '<td>' + employeesArray[i].title + '</td>' 
@@ -56,11 +56,11 @@ function appendEmployee(){
     // adding annual salary to total monthy at each employee
     totalMonthly += Number(employeesArray[i].annualSalary) / 12;
     $('#salaryTotal').empty();
-    $('#salaryTotal').append('<h2>Total Monthly:' + Number(totalMonthly).toFixed(2) + '</h2>');
+    $('#salaryTotal').append('<h3>Total Monthly:' + Number(totalMonthly).toFixed(2) + '</h3>');
     console.log(totalMonthly);
-        // if( totalMonthly > 20000){
-        //     $('$salaryTotal').css('color', 'red');
-        // }
+        if( totalMonthly > 20000){
+            $('#salaryTotal').css('color', 'red');
+        }
 
     }
     
@@ -77,6 +77,15 @@ function clearInputs(){
 
 }
 
+function removeRow(){
+    console.log('remove row');
+    console.log(this);
+    // the buttons parent is the <td> that its within, go up 1 to remove the row so parent.parent
+    $(this).parent().parent().remove();
+    
+
+}
+// could i have used a function to calculate the total monthly instead of placing it inside my append function
 // function updateTotalMonthly(salary){
 //     let totalMonthly = 0
 
